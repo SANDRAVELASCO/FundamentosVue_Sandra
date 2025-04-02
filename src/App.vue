48644f03a4e8a6a51531441dddec63c5b7d936e3
SANDRAVELASCO <itakuaa0011@gmail.com>
2025-04-01 20:04:13 -0600

    Agregando el componente Eventos y haciendo uso de eventos con los botones

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <script setup>
-import Padre from './components/Props/Padre.vue';
+import Eventos from './components/Eventos/Eventos.vue';
+
 </script>
 
 <template>
-  <!--<Horario /> -->
-  <!--<HelloWorld msg="Hola Sandra Velasco" /> -->
-  <Padre />
+    <Eventos />
+
 </template>
 <style scoped>
 
---
+++ src/components/Eventos/Eventos.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="container"> 
+        <div class="row mt-4">
+            <div>
+                <h4>Evento click</h4>
+                <!-- Botón que dispara el evento click llamando al método clic() -->
+                <button @click="clic" class="btn btn-danger" type="button"> Dale clic</button>
+                <!-- Botón que dispara otro evento click llamando al método SoyButon() -->
+                <button @click="SoyButon" class="btn btn-warning mx-2" type="button"> Dale clic 2</button>
+            </div>
+            <div>
+                <h4>Evento Change</h4>
+                <!-- Etiqueta para indicar la función del campo de entrada -->
+                <label class="input-group-text" for="">Ingrese su nombre por favor </label> 
+                <!-- Campo de entrada que dispara el evento change llamando al método nombreE() -->
+                <input class="form-control mt-2" type="text" id="nombre" @change="nombreE">               
+                <!-- Muestra el nombre ingresado en el campo de entrada -->
+                <p>El nombre ingresado es: <strong>{{nombre}}</strong> </p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            nombre: '' // Variable para almacenar el nombre ingresado por el usuario
+        }
+    },
+    methods: {
+        // Método que muestra una alerta cuando se presiona el botón rojo
+        clic(){
+            alert("Gracias por darle clic")
+        },
+        // Método que muestra una alerta cuando se presiona el botón amarillo
+        SoyButon() {
+            alert("Soy un botón")
+        },
+        // Método que captura el valor ingresado en el campo de texto y lo almacena en la variable nombre
+        nombreE(event) {
+            this.nombre = event.target.value;
+        }
+    }
+}
+</script>
+
+<style lang="">
+    
+</style>
