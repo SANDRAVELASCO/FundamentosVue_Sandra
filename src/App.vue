c099cfa06b2ee2cc7616be4f33e275adf5b250ec
SANDRAVELASCO <itakuaa0011@gmail.com>
2025-04-08 16:19:41 -0600

    Implementación de calculadora versión 2

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,13 @@
 <script setup>
 import CalculadoraV1 from './components/calculadora/CalculadoraV1.vue';
+import CalculadoraV2 from './components/calculadora/CalculadoraV2.vue';
 
 //import Eventos from './components/Eventos/Eventos.vue';
 
 </script>
 
 <template>
-    <CalculadoraV1 />
+    <CalculadoraV2 />
 
 </template>
 <style scoped>
---
+++ src/components/calculadora/CalculadoraV2.vue
@@ -0,0 +1,81 @@
+<script>
+export default{
+    data(){
+        return{
+            display: ' ',
+            currentValue:0,
+        }
+    },
+    methods:{
+        agregarNumero(numero) {
+            this.display += numero;
+        },
+        calculo() {
+            try {
+            this.currentValue = eval(this.display);
+            this.display = this.currentValue.toString();
+            } catch (error) {
+                this.display = 'Error';
+            }
+
+        },
+        limpiarDisplay(){
+        this.display =' ';
+        },
+
+        eliminarNumero(){
+            this.display = this.display.slice(0, -1);
+        },
+
+    }
+}
+</script>
+<template>
+    <div class="container d-flex justify-content-center mt-4" style="height: 350px; ">
+        <div class="card p-3" style="width: 300px; background-color: #f295fb;">
+            <div class="card-body">
+                <h6 class="text-center">CALCULADORA V2</h6>
+                <!--Display de la calculadora-->
+                <div class="mb-3">
+                    <input type="text" class="form-control" v-model="display" disabled>
+                </div>
+                <!--Botones de la calculadora-->
+                <div class="d-flex justify-content-between">
+                    <button class="btn btn-secondary" @click="">%</button>
+                    <button class="btn btn-secondary" @click="limpiarDisplay">CE</button>
+                    <button class="btn btn-secondary" @click="limpiarDisplay">C</button>
+                    <button class="btn btn-danger" @click="eliminarNumero">B</button>
+                </div> 
+
+                <div class="d-flex justify-content-between mt-2">
+                    <button class="btn btn-secondary" @click="agregarNumero(7)">7</button>
+                    <button class="btn btn-secondary" @click="agregarNumero(8)">8</button>
+                    <button class="btn btn-secondary" @click="agregarNumero(9)">9</button>
+                    <button class="btn btn-danger" @click="eliminarNumero">/</button>
+                </div> 
+
+                <div class="d-flex justify-content-between mt-2">
+                    <button class="btn btn-secondary" @click="agregarNumero(4)">4</button>
+                    <button class="btn btn-secondary" @click="agregarNumero(5)">5</button>
+                    <button class="btn btn-secondary" @click="agregarNumero(6)">6</button>
+                    <button class="btn btn-danger" @click="">X</button>
+                </div>  
+                
+                <div class="d-flex justify-content-between mt-2">
+                    <button class="btn btn-secondary" @click="agregarNumero(1)">1</button>
+                    <button class="btn btn-secondary" @click="agregarNumero(2)">2</button>
+                    <button class="btn btn-secondary" @click="agregarNumero(3)">3</button>
+                    <button class="btn btn-danger" @click="">-</button>               
+                </div>
+
+                <div class="d-flex justify-content-between mt-2">
+                    <button class="btn btn-secondary" @click="calculo">=</button>
+                    <button class="btn btn-secondary" @click="agregarNumero(0)">0</button>
+                    <button class="btn btn-secondary" @click="">.</button>
+                    <button class="btn btn-danger" @click="agregarNumero('+')">+</button>               
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<style></style>
